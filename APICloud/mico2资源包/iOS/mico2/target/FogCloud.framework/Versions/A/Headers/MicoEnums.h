a9fbf6c6adbd066589bf916466dd7330702bc672
WuZhengBin <wzbdroid@gmail.com>
2016-08-01 13:10:30 +0800

    先 commit 一下

---
+++ APICloud/mico2资源包/iOS/mico2/target/FogCloud.framework/Versions/A/Headers/MicoEnums.h
@@ -0,0 +1,16 @@
+//
+//  MicoEnums.h
+//  Pods
+//
+//  Created by WuZhengBin on 2016/7/21.
+//
+//
+
+#ifndef MicoEnums_h
+#define MicoEnums_h
+typedef enum : NSUInteger {
+    TaskTypeTimer = 0,
+    TaskTypeDelayed = 1,
+} TaskType;
+
+#endif /* MicoEnums_h */
